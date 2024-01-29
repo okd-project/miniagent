4f0df3d690a69e0c4f19807c37d1dc2f1475f282
Andrea Fasano <afasano@redhat.com>
2024-01-29 12:49:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Miniagent
+
+This repo contains a number of utility scripts to setup a virtualized OKD cluster using agent-based installer (ABI).
+The main goal is to provide a quick and easy playground for deploying OKD and experiment the various features of ABI, 
+by using the simplest approach possible (meant for educational/testing purpouses only).
+
+# Limitations
+
+The current scripts are limited to deploy just the Single Node OpenShift (SNO) topology only (so 1 node cluster).
+
+# Pre-requisites
+
+* Requires [libvirt](https://libvirt.org/compiling.html)
+    * [virt-manager](https://virt-manager.org/) is recommended
+* An ssh key stored in `~/.ssh/id_rsa.pub`
+
+# Getting started
+
+1. Launch the setup script specifying the required release version (see the [release](https://github.com/okd-project/okd/releases) page).
+
+``` bash
+$ ./sno-setup.sh quay.io/openshift/okd:4.15.0-0.okd-2024-01-27-070424
+```
+
+2. Wait for the installation to complete. The console will show a detailed output about each phase of the installation.
+
+``` bash
+...
+INFO Cluster is installed                         
+INFO Install complete!                            
+INFO To access the cluster as the system:admin user when using 'oc', run 
+INFO     export KUBECONFIG=/tmp/agent/auth/kubeconfig 
+...
+```
+
+> **_NOTE:_**  The IP of the node is `192.168.133.80`
+
+3. Connect to your new cluster using the credentials stored in the asset folder.
+
+``` bash
+$ export KUBECONFIG=/tmp/agent/auth/kubeconfig
+$ kubectl get nodes
+NAME       STATUS   ROLES                         AGE   VERSION
+master-0   Ready    control-plane,master,worker   36m   v1.28.6+0fb4726
+```
+
+Optionally, verify that the cluster has been correctly deployed:
+
+```bash
+$ kubectl get clusterversion version
+NAME      VERSION                          AVAILABLE   PROGRESSING   SINCE   STATUS
+version   4.15.0-0.okd-2024-01-27-070424   True        False         51m     Cluster version is 4.15.0-0.okd-2024-01-27-070424
+```
+
+4. Once done, to remove the cluster and cleanup the enviroment simply run the cleanup script.
+
+``` bash
+$ ./sno-cleanup.sh
+```
+
+# Troubleshooting
+
+To access the node via SSH use the following command.
+
+```bash
+$ ssh core@192.168.133.80
+```
+
+# Thanks
+
+These scripts were inspired thanks to the experience and contributions of the various authors of [dev-scripts](https://github.com/openshift-metal3/dev-scripts/)
---
+++ sno-setup.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+set -euo pipefail
+
+source "sno-common.sh"
+
+### 1. Initial checks 
+if [ $# -lt 1 ]; then
+    echo "./sno-setup.sh <release image>"
+    echo "Usage example:"
+    echo "$ ./sno-setup.sh quay.io/openshift/okd:4.15.0-0.okd-2024-01-27-070424"
+
+    exit 1
+fi
+
+releaseImage=$1
+
+if $(sudo virsh list --all | grep "\s${hostname}\s" &> /dev/null); then
+  echo "Found an already existing mini-agent instance, please run sno-cleanup.sh to remove it"
+  exit 1
+fi
+
+start=$(date +%s)
+
+### 2. Create a temporary working folder and make it accessible to the qemu and current user
+mkdir -p ${assets_dir}
+
+### 3. Get the oc binary. 
+###    This will not only be used to extract the the openshift-install binary itself from the release payload,
+###    but it will also be used internally by ABI
+if ! command -v oc &> /dev/null; then
+    echo "* Installing oc binary"
+    curl https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz | sudo tar -U -C /usr/local/bin -xzf -
+fi
+
+### 4. Get the openshift-installer
+echo "* Extracting openshift-install from ${releaseImage}"
+oc adm release extract --command=openshift-install --to=${assets_dir} ${releaseImage}
+
+### 5. Configure network, add a static mac and ip for the sno node.
+###    Some useful notes:
+###    - The domain is local to the network and will not propagate upstream.
+###    - The api DNS record points directly to SNO itself
+###    - SNO instance is configured with a static IP and MAC (so that they will be reused later when generating install config files)
+if ! $(sudo virsh net-list | grep ${network} &> /dev/null); then
+  echo "* Creating ${network} network"
+
+  cat > ${assets_dir}/${network}.xml << EOF
+<network>
+  <name>${network}</name>
+  <forward mode="nat">
+    <nat>
+      <port start="1024" end="65535"/>
+    </nat>
+  </forward>
+  <bridge name="virbr-sno" stp="on" delay="0"/>
+  <mac address="52:54:00:94:43:21"/>
+  <domain name="${domain}" localOnly="yes"/>
+  <dns>
+    <host ip="${rendezvousIP}">
+      <hostname>master-0.${domain}</hostname>
+      <hostname>${apiDomain}</hostname>
+    </host>
+  </dns>
+  <ip address="192.168.133.1" netmask="255.255.255.0">
+    <dhcp>
+      <range start="192.168.133.80" end="192.168.133.254"/>
+      <host mac="${rendezvousMAC}" name="master-0" ip="${rendezvousIP}"/>
+    </dhcp>
+  </ip>
+</network>
+EOF
+
+  sudo virsh net-define ${assets_dir}/${network}.xml
+  sudo virsh net-start ${network}
+fi
+
+###    The guest inside the agent network will not be resolvable from the host,
+###    and this will be required later by the wait-for command
+if ! $(grep "${apiDomain}" /etc/hosts &> /dev/null); then
+  echo "* Adding entry to /etc/hosts"
+  echo "${rendezvousIP} ${apiDomain}" | sudo tee -a /etc/hosts
+fi
+
+### 6. Generate the install-config.yaml and agent-config.yaml.
+###    These files will be consumed by the openshift-install later.
+echo "* Creating install config files"
+cat > ${assets_dir}/agent-config.yaml << EOF
+apiVersion: v1alpha1
+metadata:
+  name: sno 
+  namespace: mini-agent
+rendezvousIP: ${rendezvousIP}
+EOF
+
+sshKey=$(echo $(cat ~/.ssh/id_rsa.pub))
+
+cat > ${assets_dir}/install-config.yaml << EOF
+apiVersion: v1
+baseDomain: ${baseDomain}
+metadata:
+  name: sno
+  namespace: mini-agent
+controlPlane:
+  architecture: amd64
+  hyperthreading: Enabled
+  name: master
+  platform: {} 
+  replicas: 1
+compute:
+  - architecture: amd64
+    hyperthreading: Enabled
+    name: worker
+    platform: {}
+    replicas: 0
+networking:
+  clusterNetwork:
+    - cidr: 10.128.0.0/14
+      hostPrefix: 23
+  machineNetwork:
+    - cidr: 192.168.133.0/24
+  networkType: OVNKubernetes
+  serviceNetwork:
+    - 172.30.0.0/16
+platform:
+    none: {}
+pullSecret: '{"auths": {"empty": {"auth": "ZW1wdHk6ZW1wdHkK"}}}'
+sshKey: ${sshKey}
+EOF
+
+### 7. Build the agent ISO.
+echo "* Creating agent ISO"
+${assets_dir}/openshift-install agent create image --dir=${assets_dir} --log-level=debug
+
+### 8. Start the agent virtual machine 
+echo "* Launching agent SNO virtual machine"
+sudo chmod a+x ${assets_dir}
+sudo virt-install \
+  --connect 'qemu:///system' \
+  -n ${hostname} \
+  --vcpus 8 \
+  --memory 32768 \
+  --disk size=100,bus=virtio,cache=none,io=native \
+  --disk path=${assets_dir}/agent.x86_64.iso,device=cdrom,bus=sata \
+  --boot hd,cdrom \
+  --import \
+  --network network=${network},mac=${rendezvousMAC} \
+  --os-variant generic \
+  --noautoconsole &
+
+
+### 9. Check if the agent virtual machine is up and running
+while ! $(sudo virsh list --all | grep "\s${hostname}\s.*running" &> /dev/null); do
+  echo "Waiting for ${hostname} to start..."
+  sleep 5
+done
+
+### 10. Wait for the installation to complete
+${assets_dir}/openshift-install agent wait-for install-complete --dir=${assets_dir} --log-level=debug
+
+end=$(date +%s)
+echo ""
+echo "Cluster deployed in $(((end - start) / 60)) minutes"
